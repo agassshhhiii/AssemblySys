865ebc1a9fea1f7503b276f4f0b46192f3fbcf90
agassshhhiii <nastyan2022@gmail.com>
2025-01-22 17:48:44 +0700

    исправила ошибки. метод swim не получилось починить. из обсуждений с ребятами поняла, что он и не должен работать, но я ни в чем не уверена

--- src/test/java/autotests/DuckActions.java
+++ src/test/java/autotests/DuckActions.java
@@ -4,6 +4,8 @@ import com.consol.citrus.TestCaseRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import static com.consol.citrus.DefaultTestActionBuilder.action;
+import static com.consol.citrus.dsl.MessageSupport.MessageBodySupport.fromBody;
 import static com.consol.citrus.http.actions.HttpActionBuilder.http;
 
 public class DuckActions {
@@ -21,20 +23,68 @@ public class DuckActions {
                         + "  \"wingsState\": \"" + wingsState
                         + "\"\n" + "}"));
     }
+    public void createWoodDuck(TestCaseRunner runner){
+        runner.variable("color", "black");
+        runner.variable("height", "15.2");
+        runner.variable("material", "wood");
+        runner.variable("sound", "quack");
+        runner.variable("wingsState", "FIXED");
+    }
+    public void createRubberDuck(TestCaseRunner runner) {
+        runner.variable("color", "black");
+        runner.variable("height", "15.2");
+        runner.variable("material", "rubber");
+        runner.variable("sound", "quack");
+        runner.variable("wingsState", "FIXED");
+    }
+    public void idDuck(TestCaseRunner runner) {
+        runner.$(http().client("http://localhost:2222")
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .extract(fromBody().expression("$.id", "duckId")));
+    }
+    public void idCreateDuck(TestCaseRunner runner) {
+        runner.$(http().client("http://localhost:2222")
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .extract(fromBody().expression("$.id", "duckId"))
+                .body("{"
+                        + "\"id\": ${duckId},"
+                        + "\"color\": \"${color}\","
+                        + "\"height\": ${height},"
+                        + "\"material\": \"${material}\","
+                        + "\"sound\": \"${sound}\","
+                        + "\"wingsState\": \"${wingsState}\""
+                        + "}"));
+    }
+    public void checkOddDuck(TestCaseRunner runner, String id) {
+        runner.$(action(context -> {
+            String duckId = context.getVariable("duckId");
+            if (Integer.parseInt(duckId) % 2 == 0) {
+                deleteDuck(runner, "${duckId}");
+                createDuck(runner, "pink", "10", "rubber", "quack", "UNDEFINED");
+                idDuck(runner);
+            }
+        }));
+    }
+    public void checkEvenDuck(TestCaseRunner runner, String id) {
+        runner.$(action(context -> {
+            String duckId = context.getVariable("duckId");
+            if (Integer.parseInt(duckId) % 2 != 0) {
+                deleteDuck(runner, "${duckId}");
+                createDuck(runner, "pink", "10", "wood", "quack", "UNDEFINED");
+                idDuck(runner);
+            }
+        }));
+    }
     public void flyDuck(TestCaseRunner runner, String id) {
         runner.$(http()
                 .client("http://localhost:2222")
                 .send()
                 .get("/api/duck/action/fly")
                 .queryParam("id", id));
-    }
-    public void validateResponse(TestCaseRunner runner, String responseMessage) {
-        runner.$(http().client("http://localhost:2222")
-                .receive()
-                .response(HttpStatus.OK)
-                .message()
-                .contentType(MediaType.APPLICATION_JSON_VALUE).
-                body(responseMessage));
     }
     public void deleteDuck(TestCaseRunner runner, String id) {
         runner.$(http()
@@ -76,5 +126,13 @@ public class DuckActions {
                 .send()
                 .get("/api/duck/action/properties")
                 .queryParam("id", id));
+    }
+    public void validateResponse(TestCaseRunner runner, String responseMessage) {
+        runner.$(http().client("http://localhost:2222")
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .contentType(MediaType.APPLICATION_JSON_VALUE).
+                body(responseMessage));
     }
 }
