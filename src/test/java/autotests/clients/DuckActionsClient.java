23594f159ed80d46f8ef14fd4ef6c7f879af8fa9
agassshhhiii <nastyan2022@gmail.com>
2025-01-25 22:29:57 +0700

    Домашняя работа от 30 занятия. Использовала проверку через string для DeleteDuckTest, Payloads для FlyDuckTest и Resources для всех остальных тестов

--- src/test/java/autotests/clients/DuckActionsClient.java
+++ src/test/java/autotests/clients/DuckActionsClient.java
@@ -3,8 +3,12 @@ package autotests.clients;
 import autotests.EndpointConfig;
 import com.consol.citrus.TestCaseRunner;
 import com.consol.citrus.http.client.HttpClient;
+import com.consol.citrus.message.MessageType;
+import com.consol.citrus.message.builder.ObjectMappingPayloadBuilder;
 import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
@@ -19,19 +23,15 @@ public class DuckActionsClient extends TestNGCitrusSpringSupport {
     @Autowired
     protected HttpClient DuckService;
 
-    public void createDuck(TestCaseRunner runner, String color, String height, String material, String sound, String wingsState) {
+    public void createDuck(TestCaseRunner runner, Object body) {
         runner.$(http().client(DuckService)
                 .send()
                 .post("/api/duck/create")
                 .message()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body("{\n" + "  \"color\": \"" + color + "\",\n"
-                        + "  \"height\": " + height + ",\n"
-                        + "  \"material\": \"" + material + "\",\n"
-                        + "  \"sound\": \"" + sound + "\",\n"
-                        + "  \"wingsState\": \"" + wingsState
-                        + "\"\n" + "}"));
+                .body(new ObjectMappingPayloadBuilder(body, new ObjectMapper())));
     }
+
     public void getDuckId(TestCaseRunner runner) {
         runner.$(http().client(DuckService)
                 .receive()
@@ -39,43 +39,31 @@ public class DuckActionsClient extends TestNGCitrusSpringSupport {
                 .message()
                 .extract(fromBody().expression("$.id", "duckId")));
     }
-    public void validateCreateAndGetId(TestCaseRunner runner) {
-        runner.$(http().client(DuckService)
-                .receive()
-                .response(HttpStatus.OK)
-                .message()
-                .extract(fromBody().expression("$.id", "duckId"))
-                .body("{"
-                        + "\"id\": ${duckId},"
-                        + "\"color\": \"${color}\","
-                        + "\"height\": ${height},"
-                        + "\"material\": \"${material}\","
-                        + "\"sound\": \"${sound}\","
-                        + "\"wingsState\": \"${wingsState}\""
-                        + "}"));
-    }
-    public void checkOddDuck(TestCaseRunner runner) {
+
+    public void checkOddDuck(TestCaseRunner runner, Object body) {
         runner.$(action(context -> {
             String duckId = context.getVariable("duckId");
             while (Integer.parseInt(duckId) % 2 == 0) {
                 deleteDuck(runner, "${duckId}");
-                createDuck(runner, "pink", "10", "rubber", "quack", "ACTIVE");
+                createDuck(runner, body);
                 getDuckId(runner);
                 duckId = context.getVariable("duckId");
             }
         }));
     }
-    public void checkEvenDuck(TestCaseRunner runner) {
+
+    public void checkEvenDuck(TestCaseRunner runner, Object body) {
         runner.$(action(context -> {
             String duckId = context.getVariable("duckId");
             while (Integer.parseInt(duckId) % 2 != 0) {
                 deleteDuck(runner, "${duckId}");
-                createDuck(runner, "pink", "10", "wood", "quack", "ACTIVE");
+                createDuck(runner, body);
                 getDuckId(runner);
                 duckId = context.getVariable("duckId");
             }
         }));
     }
+
     public void flyDuck(TestCaseRunner runner, String id) {
         runner.$(http()
                 .client(DuckService)
@@ -83,6 +71,7 @@ public class DuckActionsClient extends TestNGCitrusSpringSupport {
                 .get("/api/duck/action/fly")
                 .queryParam("id", id));
     }
+
     public void deleteDuck(TestCaseRunner runner, String id) {
         runner.$(http()
                 .client(DuckService)
@@ -90,33 +79,37 @@ public class DuckActionsClient extends TestNGCitrusSpringSupport {
                 .delete("/api/duck/delete")
                 .queryParam("id", id));
     }
+
     public void updateDuck(TestCaseRunner runner, String id, String color, String height, String material, String sound, String wingsState) {
         runner.$(http()
                 .client(DuckService)
                 .send()
                 .put("/api/duck/update")
                 .queryParam("id", id)
-                .queryParam("color",color)
-                .queryParam("height",height)
-                .queryParam("material",material)
-                .queryParam("sound",sound)
-                .queryParam("wingsState",wingsState));
+                .queryParam("color", color)
+                .queryParam("height", height)
+                .queryParam("material", material)
+                .queryParam("sound", sound)
+                .queryParam("wingsState", wingsState));
     }
+
     public void quackDuck(TestCaseRunner runner, String id, String repetitionCount, String soundCount) {
         runner.$(http()
                 .client(DuckService)
                 .send()
                 .get("/api/duck/action/quack")
                 .queryParam("id", id)
-                .queryParam("repetitionCount",repetitionCount)
-                .queryParam("soundCount",soundCount));
+                .queryParam("repetitionCount", repetitionCount)
+                .queryParam("soundCount", soundCount));
     }
+
     public void swimDuck(TestCaseRunner runner, String id) {
         runner.$(http().client(DuckService)
                 .send()
                 .get("/api/duck/action/swim")
                 .queryParam("id", id));
     }
+
     public void propertiesDuck(TestCaseRunner runner, String id) {
         runner.$(http()
                 .client(DuckService)
@@ -124,21 +117,51 @@ public class DuckActionsClient extends TestNGCitrusSpringSupport {
                 .get("/api/duck/action/properties")
                 .queryParam("id", id));
     }
+
     public void validateResponseOk(TestCaseRunner runner, String responseMessage) {
         runner.$(http().client(DuckService)
                 .receive()
                 .response(HttpStatus.OK)
                 .message()
-                .contentType(MediaType.APPLICATION_JSON_VALUE).
-                body(responseMessage));
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new ClassPathResource(responseMessage)));
     }
-    //сделала отдельный метод, чтобы не менять все остальные тесты
-    public void validateResponse(TestCaseRunner runner, String responseMessage,HttpStatus status) {
+
+    //сделала отдельный метод для swim, чтобы не менять все остальные тесты
+    public void validateResponse(TestCaseRunner runner, String responseMessage, HttpStatus status) {
         runner.$(http().client(DuckService)
                 .receive()
                 .response(status)
                 .message()
-                .contentType(MediaType.APPLICATION_JSON_VALUE).
-                body(responseMessage));
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new ClassPathResource(responseMessage)));
+    }
+
+    public void validateCreateAndGetId(TestCaseRunner runner, String responseMessage) {
+        runner.$(http().client(DuckService)
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .extract(fromBody().expression("$.id", "duckId"))
+                .body(new ClassPathResource(responseMessage)));
+    }
+
+    //метод для проверки ответа через string в DeleteDuck
+    public void validateResponseOkDeleteDuck(TestCaseRunner runner) {
+        runner.$(http().client(DuckService)
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body("{\n" + "  \"message\": \"Duck is deleted\"\n" + "}"));
+    }
+
+    //исходя из примера в лекции с созданием утки создала файл в payload
+    public void validateResponsesPayload(TestCaseRunner runner, Object expectedPayload) {
+        runner.$(http().client(DuckService)
+                .receive()
+                .response(HttpStatus.OK)
+                .message().type(MessageType.JSON)
+                .body(new ObjectMappingPayloadBuilder(expectedPayload, new ObjectMapper())));
     }
 }
---
+++ src/test/java/autotests/payloads/PayloadsCreateDuck.java
@@ -0,0 +1,27 @@
+package autotests.payloads;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Accessors(fluent = true)
+
+public class PayloadsCreateDuck {
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("color")
+    private String color;
+    @JsonProperty("height")
+    private Double height;
+    @JsonProperty("material")
+    private String material;
+    @JsonProperty("sound")
+    private String sound;
+    @JsonProperty("wingsState")
+    private WingState wingsState;
+}
